536a58f76a79820c982e52b1426f26216ab7a86e
Daniel Musselwhite <daniel.l.musselwhite@gmail.com>
2021-12-16 15:40:23 +0000

    Created kMeans size splitter

--- Python/Experiments/Size_Splitters/k-Means/kMeans.py
+++ Python/Experiments/Size_Splitters/k-Means/kMeans.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Dec 13 15:00:15 2021
+Created on Thu Dec 16 15:31:07 2021
 
 @author: Danie
 """
 
+from sklearn.cluster import KMeans
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -33,31 +34,19 @@ for i in range(99,len(df_allRepos),1):
 
 
 
-
 #sorting in size order
 df_allRepos = df_allRepos.sort_values(by=["LOC"])
-
 #classifying all rows into sizes
-df_allRepos["Size_Classification"]=np.NaN
-
-j=0 #start of class (i is end of class)
-n=3 #number of splits
-classifier_number = 0 #classification for sizes
-stepSize = round(len(df_allRepos["LOC"])/n) # splitting into n equally sized splits
-
-#iteratively assign size classifications to equally sized sets based on the specified n splits
-for i in range(stepSize,len(df_allRepos),stepSize):
-    df_allRepos["Size_Classification"][j:i]=classifier_number
-    if(len(df_allRepos)-i>1):
-        classifier_number+=1
-    j=i
-#for the final step go from the start to the very end
-df_allRepos["Size_Classification"][j:]=classifier_number
-
-
-
+kmeans=KMeans(n_clusters=3)
+df_allRepos["Size_Classification"]= kmeans.fit_predict(df_allRepos[['LOC']])
 #saving all repos with their new size classifications
-df_allRepos.to_csv("./EqualSizeSplit_All.csv", index=False, header=True)
+df_allRepos.to_csv("./KMeansSplit_All.csv", index=False, header=True)
+
+
+
+
+
+
 
 # Visualisations and useful figures for the different sizes
 
@@ -70,7 +59,7 @@ for key in df_allRepos["Size_Classification"].unique():
 fig = plt.figure(figsize=(10,7))
 ax = fig.add_subplot(111)
 ax.set_title('Boxplot showing LOC distribution between different sized repos')
-ax.set_xlabel('Type of Repository')
+ax.set_xlabel('Repository size classification')
 ax.set_xticklabels(size_classes)
 ax.set_ylabel('LOC')
 ax.get_yaxis().get_major_formatter().set_scientific(False)
@@ -171,6 +160,4 @@ for group_name, df_sizeGroup in gb_allRepos:
     #df_bestRepos=df_bestRepos.append(df_nWorst)
     df_bestRepos=df_bestRepos.append(df_nBest)
 
-df_bestRepos.to_csv("./EqualSizeSplit_Best.csv", index=False, header=True)
-    
-
+df_bestRepos.to_csv("./kMeansSplit_Best.csv", index=False, header=True)
